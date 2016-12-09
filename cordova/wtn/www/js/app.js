0cd7c033f51bcf8a4c9e3e7419e65c2253a06568
cstests <cstests49@gmail.com>
2016-12-08 22:05:40 -0600

    update app for avatars

--- cordova/wtn/www/js/app.js
+++ cordova/wtn/www/js/app.js
@@ -24,8 +24,8 @@
 			if (page.id === 'maps') {
 				console.log('map page opened');
 				//set initial lat and long for maps page
-				var lat = 50.400064198;
-				var long = -3.505298016;
+				var lat = 0;
+				var long = 0;
 				buildMap(lat, long);
 			}
 		}
@@ -109,40 +109,60 @@
  */
 
 		function buildMap(lat, long) {
+
+				//define map
+				var map;
 				//set combined position for user
 				var latlong = new google.maps.LatLng(lat, long);
+
+
+				var startZoom  = 2;
+				var dragSet = false;
+
 				//set required options
 				var mapOptions = {
-					center: latlong,
-					zoom: 1,
 					scrollwheel: false,
+					draggable: dragSet,
 					scaleControl: false,
 					streetViewControl: false,
 					zoomControlOptions: {
 						position: google.maps.ControlPosition.TOP_RIGHT
 					},
 					backgroundColor: '#ffffff'
-				//	disableDefaultUI: true
 				};
 
 				//build map in map div...
-				var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+				map = new google.maps.Map(document.getElementById("map"), mapOptions);
+				map.setCenter(latlong);
+				map.setZoom(startZoom);
+
+      /*  map.addListener('zoom_changed', function() {
+          coordInfoWindow.setContent(createInfoWindowContent(latlong, map.getZoom()));
+          coordInfoWindow.open(map);
+        });*/
+
+				/*var iconBase = "img/avatars/";
+
 				//add initial location marker
-				//var marker = new google.maps.Marker({position: latlong,map: map});
+				var marker = new google.maps.Marker({
+					position: latlong,
+					map: map,
+					icon: iconBase + 'avatar-test1.jpg'
+				});*/
 
 				/**
- * draft test
+ * map test
  */
 
-var draftImage = new google.maps.ImageMapType({
+var mapImage = new google.maps.ImageMapType({
   getTileUrl: function(ll, z) {
     var X = ll.x % (1 << z);  // wrap
     return "maps/image4/tiles-upload/" + z + "/" + X + "/" + ll.y + ".jpg";
   },
   tileSize: new google.maps.Size(256, 256),
   isPng: false,
-  minZoom: 1,
   maxZoom: 5,
+	minZoom: startZoom,
   name: "Walking Through Naboo - Map 1",
   alt: "WTN - Map 1"
 });
@@ -150,7 +170,7 @@ var draftImage = new google.maps.ImageMapType({
 /**
  * set mapTypes to draft custom type
  */
-map.mapTypes.set('draft', draftImage);
+map.mapTypes.set('draft', mapImage);
 map.setMapTypeId('draft');
 
 map.setOptions({
@@ -161,7 +181,106 @@ map.setOptions({
   }
 });
 
-			}
+/**
+* geocoder
+*/
+var geocoder = new google.maps.Geocoder();
+
+function geocode(opts) {
+  function geocodeResult(response, status) {
+    if (status == google.maps.GeocoderStatus.OK && response[0]) {
+      map.fitBounds(response[0].geometry.viewport);
+    }
+  } // trim leading and trailing space with trim capable browsers
+  if(opts.address && opts.address.trim) opts.address = opts.address.trim();
+  if(opts.address || opts.latLng) geocoder.geocode(opts, geocodeResult); // no empty request
+}
+
+var strictBounds;
+
+/*check and limit map and viewport area for drag event*/
+google.maps.event.addListenerOnce(map, 'tilesloaded', function() {
+
+var allowedBounds = map.getBounds();
+
+console.log("allowed bounds = "+allowedBounds);
+
+var allowed_ne_lng = allowedBounds.getNorthEast().lng();
+var allowed_ne_lat = allowedBounds.getNorthEast().lat();
+var allowed_sw_lng = allowedBounds.getSouthWest().lng();
+var allowed_sw_lat = allowedBounds.getSouthWest().lat();
+
+// SW first, then NE corner
+strictBounds = new google.maps.LatLngBounds(
+  new google.maps.LatLng(allowed_sw_lat, allowed_sw_lng),
+  new google.maps.LatLng(allowed_ne_lat, allowed_ne_lng)
+);
+
+    zoomLevel = map.getZoom();
+
+    google.maps.event.addListener(map,'center_changed',function() {
+    checkBounds(allowedBounds);
+		console.log("center_changed fired");
+    });
+
+});
+
+google.maps.event.addListener(map, 'drag', checkBounds);
+
+// Limit map area
+function checkBounds(allowedBounds) {
+
+var currentBounds = map.getBounds();
+
+var current_ne_lng = currentBounds.getNorthEast().lng();
+var current_ne_lat = currentBounds.getNorthEast().lat();
+var current_sw_lng = currentBounds.getSouthWest().lng();
+var current_sw_lat = currentBounds.getSouthWest().lat();
+
+console.log("drag event fired...");
+
+}
+/*end of drag check*/
+
+// Listen for the dragend event
+            google.maps.event.addListener(map, 'dragend', function() {
+
+                if (strictBounds.contains(map.getCenter())) return;
+
+                // out of bounds - move map within bounds
+                var c = map.getCenter(),
+                x = c.lng(),
+                y = c.lat(),
+                maxX = strictBounds.getNorthEast().lng(),
+                maxY = strictBounds.getNorthEast().lat(),
+                minX = strictBounds.getSouthWest().lng(),
+                minY = strictBounds.getSouthWest().lat();
+
+                if (x < minX) x = minX;
+                if (x > maxX) x = maxX;
+                if (y < minY) y = minY;
+                if (y > maxY) y = maxY;
+
+                map.setCenter(new google.maps.LatLng(0, 0));
+
+
+
+            });
+
+/*zoom level check*/
+google.maps.event.addListener(map, 'zoom_changed', function() {
+    zoomLevel2 = map.getZoom();
+
+    if (zoomLevel2 == startZoom) {
+    map.setOptions({draggable:false});
+    }
+    else if (zoomLevel2 > startZoom) {
+   map.setOptions({draggable: true});
+    }
+
+});
+
+}
 
 		//onsen - init event is fired after ons-page attached to DOM...
 		document.addEventListener('init', onsInit, false);
